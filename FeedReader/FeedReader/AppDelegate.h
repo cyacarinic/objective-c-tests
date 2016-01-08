1b9557dc2aa6985dc83c2ca6b71c0a3bc870bb74
Claudio Yacarini <cyacarini@rcp.pe>
2016-01-08 12:44:35 -0500

    nuevo proyecto Feed Reader

---
+++ FeedReader/FeedReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FeedReader
+//
+//  Created by Claudio Yacarini on 8/01/16.
+//  Copyright © 2016 Claudio Yacarini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FeedReader/FeedReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FeedReader
+//
+//  Created by Claudio Yacarini on 8/01/16.
+//  Copyright © 2016 Claudio Yacarini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
