113b3bd4578ed66c760eb8122f1f000fc87df8e1
Claudio Yacarini <cyacarini@rcp.pe>
2016-01-08 12:06:18 -0500

    menú operativo

---
+++ SlideOutMenu/SlideOutMenu/NavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationViewController.h
+//  SlideOutMenu
+//
+//  Created by Claudio Yacarini on 7/01/16.
+//  Copyright © 2016 Claudio Yacarini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationViewController : UITableViewController
+
+@end
---
+++ SlideOutMenu/SlideOutMenu/NavigationViewController.m
@@ -0,0 +1,73 @@
+//
+//  NavigationViewController.m
+//  SlideOutMenu
+//
+//  Created by Claudio Yacarini on 7/01/16.
+//  Copyright © 2016 Claudio Yacarini. All rights reserved.
+//
+
+#import "NavigationViewController.h"
+#import "SWRevealViewController.h"
+
+@interface NavigationViewController ()
+
+@end
+
+@implementation NavigationViewController {
+    NSArray *menu;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    menu = @[@"first", @"second", @"tercero"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    return [menu count];
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    NSString *cellIdentifier = [menu objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
+    if ( [segue isKindOfClass: [SWRevealViewControllerSegue class]] ) {
+        SWRevealViewControllerSegue *swSegue = (SWRevealViewControllerSegue*) segue;
+        
+        swSegue.performBlock = ^(SWRevealViewControllerSegue* rvc_segue, UIViewController* svc, UIViewController* dvc) {
+            
+            UINavigationController* navController = (UINavigationController*)self.revealViewController.frontViewController;
+            [navController setViewControllers: @[dvc] animated: NO];
+            [self.revealViewController setFrontViewPosition: FrontViewPositionLeft animated: YES];
+        };
+        
+    }
+    
+}
+
+
+@end
