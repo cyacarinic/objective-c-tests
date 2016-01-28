58ab4c59a1e554661ef35f7329b4a837a144e47d
Claudio Yacarini <cyacarini@rcp.pe>
2016-01-28 10:28:24 -0500

    se cargan las compras guardadas

--- InAppPurchaseExample/InAppPurchaseExample/ViewController.m
+++ InAppPurchaseExample/InAppPurchaseExample/ViewController.m
@@ -6,6 +6,8 @@
 //  Copyright © 2016 Claudio Yacarini. All rights reserved.
 //
 
+#define k_save @"Saveitem"
+
 #import "ViewController.h"
 
 @interface ViewController ()
@@ -16,7 +18,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Se carga la vista de "ya fue comprado"
+    NSUserDefaults *saveApp = [NSUserDefaults standardUserDefaults];
+    bool saved = [saveApp boolForKey:k_save];
+    if(!saved){
+        // Código para compra no guardado
+        Label.text = @"No hay compras guardadas.";
+    }else{
+        // Código para compra YA realizada y guardada
+        Label.text = @"El item ya ha sido comprado!! Se cargó la compra guardada. =)";
+    }
 }
 
 - (void)didReceiveMemoryWarning {
@@ -27,12 +39,20 @@
 - (IBAction)purchaseItem:(id)sender {
     _purchaseController = [[PurchasedViewController alloc] initWithNibName:nil bundle:nil];
     _purchaseController.productID = @"pe.rcp.test.iap1";
+    
+    // Si ya se compró el item se pre-carga la compra
+    [[SKPaymentQueue defaultQueue] addTransactionObserver:_purchaseController];
+    
     [self presentViewController:_purchaseController animated:YES completion:NULL];
     [_purchaseController getProductID:self];
 }
 
 - (void) Purchased {
     Label.text = @"El item ha sido comprado!! =)";
+    
+    NSUserDefaults *saveApp = [NSUserDefaults standardUserDefaults];
+    [saveApp setBool:TRUE forKey:k_save];
+    [saveApp synchronize];
 }
 
 @end
